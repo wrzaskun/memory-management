a8dba4fc597101f6aa4933432d27fa62c2634c69
Michał Przymusiński <wrzaskun000@gmail.com>
2021-10-04 00:05:30 +0200

    add shared and unique ptr for list

--- exercises/List.cpp
+++ exercises/List.cpp
@@ -1,99 +1,80 @@
 #include <iostream>
-
+#include <memory>
 using namespace std;
 
 class Node
 {
 public:
-    Node(const int v) : next(nullptr), value(v) {}
+    Node(const int v) : value(v) {}
 
-    Node *next;
+    shared_ptr<Node> next;
     int value;
-    ~Node(){
-        if(next !=nullptr){
-            delete next;
-        }
-    }
 };
 
 class List
 {
 public:
-    List();
-    void add(Node *node);
-    Node *get(const int value);
+    List(){};
 
-    virtual ~List()
+    void add(unique_ptr<Node> node)
     {
-        if (first != nullptr)
+        if (first == nullptr)
         {
-            delete first;
+            first = std::move(node);
+        }
+        else
+        {
+            shared_ptr<Node> current = first;
+            while (current->next)
+            {
+                current = (current->next);
+            }
+            current->next = std::move(node);
+        }
+    };
+
+    shared_ptr<Node> get(const int value)
+    {
+        if (!first)
+        {
+            cout << "List is empty!" << endl;
+            return nullptr;
+        }
+        else
+        {
+            shared_ptr<Node> current = (first);
+            do
+            {
+                if (current->value == value)
+                {
+                    cout << "Found value " << current->value << endl;
+                    return current;
+                }
+                else
+                {
+                    cout << "Going through " << current->value << endl;
+                    current = (current->next);
+                }
+            } while (current);
+            cout << "Not found: value " << value << endl;
+            return nullptr;
         }
     }
 
 private:
-    Node *first = nullptr;
+    shared_ptr<Node> first;
 };
-
-List::List() : first(nullptr)
-{
-}
-
-void List::add(Node *node)
-{
-    if (first == nullptr)
-    {
-        first = node;
-    }
-    else
-    {
-        Node *current = first;
-        while (current->next)
-        {
-            current = current->next;
-        }
-        current->next = node;
-    }
-}
-
-Node *List::get(const int value)
-{
-    if (!first)
-    {
-        cout << "List is empty!" << endl;
-        return nullptr;
-    }
-    else
-    {
-        Node *current = first;
-        do
-        {
-            if (current->value == value)
-            {
-                cout << "Found value " << current->value << endl;
-                return current;
-            }
-            else
-            {
-                cout << "Going through " << current->value << endl;
-                current = current->next;
-            }
-        } while (current);
-        cout << "Not found: value " << value << endl;
-        return nullptr;
-    }
-}
 
 int main()
 {
     List lista;
-    Node *node4 = new Node(4);
-    Node *node7 = new Node(7);
+    auto node4 = make_unique<Node>(4);
+    auto node7 = make_unique<Node>(7);
 
-    lista.add(node4);
-    lista.add(new Node(2));
-    lista.add(node7);
-    lista.add(new Node(9));
+    lista.add(std::move(node4));
+    lista.add(make_unique<Node>(2));
+    lista.add(std::move(node7));
+    lista.add(make_unique<Node>(9));
     auto node = lista.get(1);
 
     if (node)
